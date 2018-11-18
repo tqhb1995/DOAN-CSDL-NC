3e4a4382a13e5b5496e3f3c912c8a85d9491466e
Toan <duongminhtoan16@gmail.com>
2018-11-18 19:58:05 +0700

    update
    Update code

--- Database-Trigger-function-procedure/Function/Function.sql
+++ Database-Trigger-function-procedure/Function/Function.sql
@@ -23,3 +23,106 @@ END
 
 ALTER TABLE dbo.KhachHang
 ADD DEFAULT dbo.Auto_IdKH() FOR maKH
+
+--Tạo mã nhân viên tự động
+CREATE FUNCTION Auto_IdNV()
+RETURNS CHAR(10)
+AS
+BEGIN
+	DECLARE @id varCHAR(10)
+	DECLARE  @temp int
+	IF (SELECT COUNT(maNV) FROM dbo.NhanVien) = 0
+		SET @temp = 0
+	ELSE
+		SET @temp = (SELECT MAX(CONVERT(INT, (RIGHT(maKH, 8)))) FROM dbo.KhachHang)
+	SET @id = CASE
+		WHEN @temp >= 0 and @temp < 9 THEN 'NV0000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9 AND @temp < 99 THEN 'NV000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99 AND @temp < 999 THEN 'NV00000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999 AND @temp < 9999 THEN 'NV0000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9999 AND @temp < 99999 THEN 'NV000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99999 AND @temp < 999999 THEN 'NV00' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999999 AND @temp < 9999999 THEN 'NV0' + CONVERT(CHAR, @temp + 1)
+		END
+     RETURN @id
+END
+
+ALTER TABLE dbo.NhanVien
+ADD DEFAULT dbo.Auto_IdNV() FOR maNV
+
+-- Function tạo mã Khách Sạn
+CREATE FUNCTION Auto_IdKS()
+RETURNS CHAR(10)
+AS
+BEGIN
+	DECLARE @id varCHAR(10)
+	DECLARE  @temp int
+	IF (SELECT COUNT(maKS) FROM dbo.KhachSan) = 0
+		SET @temp = 0
+	ELSE
+		SET @temp = (SELECT MAX(CONVERT(INT, (RIGHT(maKS, 8)))) FROM dbo.KhachSan)
+	SET @id = CASE
+		WHEN @temp >= 0 and @temp < 9 THEN 'KS0000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9 AND @temp < 99 THEN 'KS000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99 AND @temp < 999 THEN 'KS00000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999 AND @temp < 9999 THEN 'KS0000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9999 AND @temp < 99999 THEN 'KS000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99999 AND @temp < 999999 THEN 'KS00' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999999 AND @temp < 9999999 THEN 'KS0' + CONVERT(CHAR, @temp + 1)
+		END
+     RETURN @id
+END
+
+ALTER TABLE dbo.KhachSan
+ADD DEFAULT dbo.Auto_IdKS() FOR maKS
+--fUNCTION Tạo mã tự động hóa đơn
+CREATE FUNCTION Auto_IdHD()
+RETURNS CHAR(10)
+AS
+BEGIN
+	DECLARE @id varCHAR(10)
+	DECLARE  @temp int
+	IF (SELECT COUNT(maHD) FROM dbo.HoaDon) = 0
+		SET @temp = 0
+	ELSE
+		SET @temp = (SELECT MAX(CONVERT(INT, (RIGHT(maHD, 8)))) FROM dbo.HoaDon)
+	SET @id = CASE
+		WHEN @temp >= 0 and @temp < 9 THEN 'HD0000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9 AND @temp < 99 THEN 'HD000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99 AND @temp < 999 THEN 'HD00000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999 AND @temp < 9999 THEN 'HD0000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9999 AND @temp < 99999 THEN 'HD000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99999 AND @temp < 999999 THEN 'HD00' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999999 AND @temp < 9999999 THEN 'HD0' + CONVERT(CHAR, @temp + 1)
+		END
+     RETURN @id
+END
+
+ALTER TABLE dbo.HoaDon
+ADD DEFAULT dbo.Auto_IdHD() FOR maHD
+
+--Function tạo mã đặt phòng--
+CREATE FUNCTION Auto_IdDP()
+RETURNS CHAR(10)
+AS
+BEGIN
+	DECLARE @id varCHAR(10)
+	DECLARE  @temp int
+	IF (SELECT COUNT(maDP) FROM dbo.DatPhong) = 0
+		SET @temp = 0
+	ELSE
+		SET @temp = (SELECT MAX(CONVERT(INT, (RIGHT(maDP, 8)))) FROM dbo.DatPhong)
+	SET @id = CASE
+		WHEN @temp >= 0 and @temp < 9 THEN 'DP0000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9 AND @temp < 99 THEN 'DP000000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99 AND @temp < 999 THEN 'DP00000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999 AND @temp < 9999 THEN 'DP0000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 9999 AND @temp < 99999 THEN 'DP000' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 99999 AND @temp < 999999 THEN 'DP00' + CONVERT(CHAR, @temp + 1)
+		WHEN @temp >= 999999 AND @temp < 9999999 THEN 'DP0' + CONVERT(CHAR, @temp + 1)
+		END
+     RETURN @id
+END
+
+ALTER TABLE dbo.DatPhong
+ADD DEFAULT dbo.Auto_IdDP() FOR maDP
--- Database-Trigger-function-procedure/Procedure/Procedure.sql
+++ Database-Trigger-function-procedure/Procedure/Procedure.sql
@@ -1,12 +1,13 @@
---Dang kí khách hàng
+--Đăng kí khách hàng
 CREATE PROCEDURE DangKiKH
 	@hoTen nvarchar(100), @tenDangNhap varCHAR(30), @matKhau varCHAR(16), @soCMND varCHAR(12), @diaChi nvarchar(200), @soDienThoai varCHAR(13), @moTa nvarchar(1000), @email varCHAR(100)
    AS
-   IF(EXISTS(SELECT * FROM dbo.KhachHang WHERE tenDangNhap = @tenDangNhap))
+   IF(EXISTS(SELECT * FROM dbo.KhachHang WHERE tenDangNhap = @tenDangNhap OR soCMND = @soCMND OR soDienThoai = @soDienThoai OR email = @email))
    BEGIN
    RAISERROR('Lỗi!',16,1)
    RETURN
    END
+
    INSERT INTO dbo.KhachHang
            ( hoTen ,
              tenDangNhap ,
@@ -26,3 +27,37 @@ CREATE PROCEDURE DangKiKH
              @moTa , -- moTa - nvarchar(30)
              @email  -- email - char(20)
            )
+
+
+
+--Thêm khách sạn
+
+CREATE PROCEDURE ThemKhachSan
+  (@tenKS nvarchar(100), @soSao CHAR(2), @soNha varCHAR(10), @duong nvarCHAR(100), @quan nvarchar(100), @thanhPho nvarCHAR(100), @moTa nvarchar(1000), @giaTB int)
+   AS
+   INSERT INTO dbo.KhachSan
+           ( tenKS ,
+             soSao ,
+             soNha ,
+             duong ,
+             quan ,
+             thanhPho ,
+             moTa ,
+             giaTB
+           )
+   VALUES  ( @tenKS , 
+             @soSao , 
+             @soNha , 
+             @duong , 
+             @quan , 
+             @thanhPho , 
+             @moTa , 
+             @giaTB  
+           )
+
+--Procedure Đăng nhập hệ thống
+create procedure DangNhapHeThong
+  @tenDangNhap varchar(30), @matKhau varchar(16)
+as
+  Select * from KhachHang
+      where tenDangNhap = @tenDangNhap and matKhau = @matKhau
